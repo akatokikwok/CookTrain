e2fac000e1cdd623b83001d81ac083ad896fa7ea
akatokikwok <42242472+akatokikwok@users.noreply.github.com>
2020-03-26 17:28:44 +0800

    BarracksActor

---
+++ BarracksTemplate/Source/Orbiting/BarracksUnit.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BarracksUnit.h"
+
+#include "Barracks.h"
+
+// Sets default values
+ABarracksUnit::ABarracksUnit()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	VisualRepresentation =
+		CreateDefaultSubobject<UParticleSystemComponent>("SpawnPoint");
+	auto ParticleSystem =ConstructorHelpers::FObjectFinder<UParticleSystem>(TEXT("ParticleSystem'/Engine/Tutorial/SubEditors/TutorialAssets/TutorialParticleSystem.TutorialParticleSystem'"));
+	if (ParticleSystem.Object != nullptr)
+	{
+		VisualRepresentation->SetTemplate(ParticleSystem.Object);
+	}
+
+	VisualRepresentation->SetRelativeScale3D(FVector(0.5, 0.5, 0.5));
+
+	SpawnCollisionHandlingMethod =ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+
+}
+
+// Called when the game starts or when spawned
+void ABarracksUnit::BeginPlay()
+{
+	Super::BeginPlay();
+	VisualRepresentation->AttachTo(RootComponent);
+
+}
+
+// Called every frame
+void ABarracksUnit::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetActorLocation(GetActorLocation() + FVector(10, 0, 0));
+}
+
+// Called to bind functionality to input
+void ABarracksUnit::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ BarracksTemplate/Source/Orbiting/OrbitingMovementComponent.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "OrbitingMovementComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ORBITING_API UOrbitingMovementComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOrbitingMovementComponent();
+
+	UPROPERTY()
+	bool RotateToFaceOutwards;
+
+	UPROPERTY()
+	float RotationSpeed;
+
+	UPROPERTY()
+	float OrbitDistacne;
+	float CurrentValue;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
